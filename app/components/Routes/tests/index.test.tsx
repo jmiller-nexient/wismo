34ca55e6a7acf81566b709b495fc16236120a929
gaber mowiena <gmowiena@nexient.com>
2020-04-09 11:33:13 -0400

    adding tests

---
+++ app/components/Routes/tests/index.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Routes from '..';
+import Layout from 'components/Layout';
+import NotFoundPage from 'components/NotFoundPage';
+import { MemoryRouter } from 'react-router';
+
+describe('routes using memory router', () => {
+    it('should show Home component for / router (using memory router)', () => {
+        const component = mount(<MemoryRouter initialEntries={['/']} >
+            <Routes />
+        </MemoryRouter>
+        );
+        expect(component.find(Layout)).toHaveLength(1);
+    });
+    it('should show No match component for route not defined', () => {
+        const component = mount(<MemoryRouter initialEntries={['/unknown']} >
+            <Routes />
+        </MemoryRouter>
+        );
+        expect(component.find(NotFoundPage)).toHaveLength(1);
+    });
+});

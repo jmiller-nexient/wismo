f307d72e8db879c5a1f3cd00c7e62374f6855f56
jmiller-nexient <62888982+jmiller-nexient@users.noreply.github.com>
2020-04-17 00:24:09 -0400

    US-1093: Added banner and updated status page layout to match wireframes (#33)

--- app/components/Footer/tests/index.test.tsx
+++ app/components/Footer/tests/index.test.tsx
@@ -1,11 +1,13 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import renderer from 'react-test-renderer';
+
+import TestRendererJsonType from 'types/TestRendererJsonType';
 
 import Footer from '..';
 
 describe('<Footer />', () => {
   it('should render correctly', () => {
-    const tree: ReactElement = renderer.create(
+    const tree: TestRendererJsonType = renderer.create(
       <Footer />,
     ).toJSON();
 
--- app/components/OrderStatusPage/index.tsx
+++ app/components/OrderStatusPage/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
+import CustomerName from 'components/CustomerName';
 import Footer from 'components/Footer';
+import Hyperlink from 'components/Hyperlink';
+import PhoneNumber from 'components/PhoneNumber';
+import ServiceRequestBanner from 'components/ServiceRequestBanner';
+import ServiceRequestViewHeader from 'components/ServiceRequestViewHeader';
 
-const OrderStatusPage: React.FC = (): React.ReactElement => {
-  return (
-    <div>
-      <div>Order Status</div>
-      <Footer />
+import './index.less';
+
+const OrderStatusPage: React.FC = (): React.ReactElement => (
+  <>
+    <header className="page-header">&lt;PageHeader /&gt; goes here</header>
+    <div className="dte-wismo-order-status-page">
+      <ServiceRequestViewHeader serviceRequestNumber="867530986753" />
+      <ServiceRequestBanner serviceDate="2020-04-22" />
+      <div className="stepper-title">Start Service Request</div>
+      <div className="stepper">Stepper goes here</div>
+      <div className="customer-name">
+        <CustomerName firstName="Adam" lastName="Naglich" />
+      </div>
+      <div>
+        <span className="phone-number">
+          <PhoneNumber value="1234567890" />
+        </span>
+        <Hyperlink href="/orders/678">Edit Number</Hyperlink>
+      </div>
+      <div className="service-summary">&lt;ServiceSummary /&gt; goes here</div>
     </div>
-  );
-};
+    <Footer />
+  </>
+);
 
 export default OrderStatusPage;

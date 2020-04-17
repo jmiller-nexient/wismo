c660c3eace5d84d2ac4514b0531f54f2b27e2896
Alexander Stone <astone@LT-C02YV7G7LVCG.attlocal.net>
2020-04-16 20:26:25 -0400

    US-1051: WIP: Added basic structure for desktop and mobile setup. Hamburger menu is currently non-functional. Images do not work.

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
 
+import Hyperlink from 'components/Hyperlink';
+
 import './index.less';
 
 const Header: React.FC = (): React.ReactElement => (
     <header className="dte-wismo-header-background">
         <div className="dte-wismo-header-mobile">
-            Mobile
+            <div className="dte-wismo-hamburger">
+                <img src="../../images/Hamburger_Menu.svg"/>
+            </div>
+            <div>
+                <img className="dte-wismo-header-logo" src="../../images/DTE_Wordmark.svg" />
+            </div>
+            <div className="dte-wismo-header-page-title">
+                Status
+            </div>
         </div>
         <div className="dte-wismo-header-desktop">
-            Desktop
+            <div className="dte-wismo-header-page-title">
+                Status
+            </div>
+            <div>
+                <img className="dte-wismo-header-logo" src="../../images/DTE_Wordmark.svg" />
+            </div>
+            <div className="dte-wismo-header-link">
+                <Hyperlink href="www.dteenergy.com">Visit DTEEnergy.com</Hyperlink>
+            </div>
         </div>
     </header>
 );
